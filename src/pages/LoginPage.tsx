4ff9dca0151c90702bbb32c51d40a1ccc445b6a2
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-05-21 04:06:18 -0400

    login of user

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,20 +1,32 @@
-import React from "react";
-import { Button, Form, Input } from "antd";
+import React, { useState } from "react";
+import { Alert, Button, Form, Input } from "antd";
 import Link from "next/link";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function LoginPage() {
+  const router = useRouter();
+  const [alertText, setAlertText] = useState<string>("");
   const onFinish = (values: any) => {
+    const userDetails = {
+      email: values.email,
+      password: values.password,
+    };
     axios
       .post("http://localhost:8000/login", {
-        values,
+        userDetails,
       })
       .then(function (response) {
         console.log(response.data);
+        if (response.data.message === "Login Successful") {
+          router.push("/Dashboard");
+        }
       })
       .catch(function (error) {
-        console.log(error);
+        if (error.response.data.message === "User Not Found") {
+          setAlertText("User Not Found");
+        } else if (error.response.data.message === "Wrong Password") setAlertText("Wrong Password");
       });
   };
 
@@ -23,6 +35,21 @@ export default function LoginPage() {
   };
   return (
     <>
+      {alertText.length ? (
+        <div className="z-10 fixed p-7 ml-24">
+          <Alert
+            message="Error"
+            description={alertText}
+            type="error"
+            showIcon
+            closable
+            afterClose={() => setAlertText("")}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
+
       <div className="backGroundImage bg-cover bg-center flex items-start  justify-around flex-col h-screen">
         <Form
           name="basic"
@@ -37,7 +64,7 @@ export default function LoginPage() {
           <Form.Item
             name="email"
             className=" flex justify-around"
-            rules={[{ required: true, message: "Please input your Email!" }]}
+            rules={[{ required: true, message: "Email is required!" }]}
           >
             <Input
               className="w-64"
@@ -49,7 +76,10 @@ export default function LoginPage() {
           <Form.Item
             name="password"
             className=" flex  justify-around"
-            rules={[{ required: true, message: "Please input your password!" }]}
+            rules={[
+              { required: true, message: "Password is required!" },
+              { min: 8, message: "Password must be minimum 8 characters." },
+            ]}
           >
             <Input
               className="w-64"
