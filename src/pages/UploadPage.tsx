1f2bb075dd4a7a193a4120dc9b1926a54f6a28b5
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-06-18 01:17:46 +0530

    upload file operations

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -19,22 +19,25 @@ const dummyRequest = async () => {
 const props: UploadProps = {
   name: "file",
   multiple: true,
-  customRequest: dummyRequest,
+  action: "http://localhost:8000/uploadfile",
   onChange(info) {
     console.log(info);
     const { status } = info.file;
     console.log(status);
-    // if (status !== "uploading") {
-    //   console.log(info.file, info.fileList);
-    // }
-    // if (status === "done") {
-    //   message.success(`${info.file.name} file uploaded successfully.`);
-    // } else if (status === "error") {
-    //   message.error(`${info.file.name} file upload failed.`);
-    // }
+    if (status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
   },
   onDrop(e) {
     console.log("Dropped files", e.dataTransfer.files);
+  },
+  onRemove(file) {
+    console.log(file.uid);
   },
 };
 
