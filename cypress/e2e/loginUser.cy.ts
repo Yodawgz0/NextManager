1bed88d08f86f130b752ac42a04aa10bac122e38
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-07-21 23:16:29 +0530

    adding testing script

---
+++ cypress/e2e/loginUser.cy.ts
@@ -0,0 +1,12 @@
+import { constants } from "../constant";
+
+describe("Login Page", () => {
+  it("Does the login", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Login").click();
+    cy.url().should("include", "/LoginPage");
+    cy.get("input[placeholder='Email']").type(constants.username);
+    cy.get("input[placeholder='Password']").type(constants.password);
+    cy.contains("Login").click();
+  });
+});
