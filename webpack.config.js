dee47b7efd01a8cb748c6785d960fefcdcf49654
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-08-02 01:21:57 -0400

    webpack dev

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,29 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main-bundle.js",
   },
+  rules: [
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+    },
+    {
+      test: /\.(t|j)sx?$/,
+      use: { loader: "ts-loader" },
+      exclude: /node_modules/,
+    },
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
 };
