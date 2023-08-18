688974dc300771ee9418cb3a2456b4a4ee1da18c
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-08-17 23:07:11 -0400

    fetching data from the server

--- src/components/FilterTable.tsx
+++ src/components/FilterTable.tsx
@@ -1,32 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input, Select } from "antd";
+import axios from "axios";
 const { Search } = Input;
-
+axios.defaults.withCredentials = true;
 function FilterTable() {
   const [loadingSearchName, setLoadingSearchName] = useState<boolean>(false);
   const handleChange = (value: string) => {
     console.log(`selected ${value}`);
   };
 
+  const [playerNamesUniq, setPlayerNamesUniq] = useState<
+    { [key: string]: string }[]
+  >([]);
+
+  useEffect(() => {
+    // axios
+    //   .get("http://localhost:8000/playerDataFilter")
+    //   .then((res) => console.log(res));
+    axios
+      .get("http://localhost:8000/playerDataFilter/getplayernames")
+      .then((res) => setPlayerNamesUniq(res.data.data[0].names));
+  }, []);
+
   return (
     <>
       <div className="px-11 bg-amber-100 d-flex flex-row">
         <Search
-          className=" bg-violet-950 mt-3 rounded-lg"
+          className=" bg-violet-950 mt-3 rounded-lg w-96"
           placeholder="input search text"
           enterButton="Search"
-          size="large"
           loading={loadingSearchName}
         />
         <Select
-          defaultValue="lucy"
+          defaultValue="Please choose one..."
           onChange={handleChange}
-          options={[
-            { value: "jack", label: "Jack" },
-            { value: "lucy", label: "Lucy" },
-            { value: "Yiminghe", label: "yiminghe" },
-            { value: "disabled", label: "Disabled", disabled: true },
-          ]}
+          className="mt-3 ms-5 w-64"
+          options={playerNamesUniq.map((item) => ({
+            value: item,
+            label: item,
+          }))}
         />
       </div>
     </>
