43f6f19cc55117e49c143965d202f3b4888f2f54
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-09-26 12:44:38 -0400

    adding a testing script which is ultra

--- cypress/e2e/loginUser.cy.ts
+++ cypress/e2e/loginUser.cy.ts
@@ -1,6 +1,7 @@
-import { constants } from "../constant";
+// @ts-nocheck
 
-export function login() {
+import { constants } from "../constant";
+describe("My First Test", () => {
   it("Does the login", () => {
     cy.visit("http://localhost:3000");
     cy.contains("Login").click();
@@ -9,4 +10,29 @@ export function login() {
     cy.get("input[placeholder='Password']").type(constants.password);
     cy.contains("Login").click();
   });
-}
+});
+
+// describe("Automating", () => {
+//   it("Download", () => {
+//     cy.visit("https://culturalanalytics.org/articles");
+//     cy.wait(3000);
+//     let alts = [];
+//     cy.get("img")
+//       .each(($img) => {
+//         let alt = $img.attr("alt");
+//         alts.push(alt);
+//       })
+//       .then(() => {
+//         for (let alt of alts) {
+//           cy.get(`img[alt="${alt}"]`).click();
+//           cy.get(".banner-page-header").invoke("remove");
+//           cy.get("#article-citation").invoke("remove");
+//           cy.get(".journal").invoke("remove");
+//           cy.contains("Save article").click();
+//           cy.contains("XML").click();
+//           cy.wait(8000);
+//           cy.go("back");
+//         }
+//       });
+//   });
+// });
