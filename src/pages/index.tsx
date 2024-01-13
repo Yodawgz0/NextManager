dea71830c098d9bb0f3a9975a4bad9b9d9f19975
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-01-13 13:25:27 -0500

    removing single component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,23 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div className="backGroundImage bg-cover bg-center flex items-start  justify-around flex-col h-screen">
-        <Space
-          direction="vertical"
-          style={{ width: "20%", marginLeft: "5rem" }}
-          className="flex"
-        >
-          <Link href="/LoginPage">
-            <Button className="m-4" block name="Login" type="primary" ghost>
-              Login
-            </Button>
-          </Link>
-          <Link href="/RegisterPage">
-            <Button className="m-4" name="Register" block danger>
-              Register
-            </Button>
-          </Link>
-        </Space>
-      </div>
-    </>
+    <div className="backGroundImage bg-cover bg-center flex items-start  justify-around flex-col h-screen">
+      <Space
+        direction="vertical"
+        style={{ width: "20%", marginLeft: "5rem" }}
+        className="flex"
+      >
+        <Link href="/LoginPage">
+          <Button className="m-4" block name="Login" type="primary" ghost>
+            Login
+          </Button>
+        </Link>
+        <Link href="/RegisterPage">
+          <Button className="m-4" name="Register" block danger>
+            Register
+          </Button>
+        </Link>
+      </Space>
+    </div>
   );
 }
