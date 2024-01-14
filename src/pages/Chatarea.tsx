bdb32e15d4731815a63e49cfcc4a741512ba1bc2
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-01-13 23:50:19 -0500

    removing single enclosures

--- src/pages/Chatarea.tsx
+++ src/pages/Chatarea.tsx
@@ -8,7 +8,7 @@ const socket = new WebSocket("ws://localhost:7000");
 const Chatarea: React.FC = () => {
   const onExitChat = () => {
     router.push("/Dashboard");
-      socket.addEventListener("close", (event) => {
+    socket.addEventListener("close", (event) => {
       console.log("WebSocket connection closed", event);
     });
     return () => {
@@ -16,9 +16,9 @@ const Chatarea: React.FC = () => {
     };
   };
   useEffect(() => {
-      socket.addEventListener("open", (event) => {
+    socket.addEventListener("open", (event) => {
       console.log("WebSocket connection opened", event);
-    }); 
+    });
     socket.addEventListener("message", (event) => {
       console.log("Received message:", event.data);
     });
@@ -27,37 +27,35 @@ const Chatarea: React.FC = () => {
     };
   }, []);
   return (
-    <>
-      <div className=" h-screen bg-[#0e151b] p-5">
-        <div className="flex flex-row h-[99%] bg-[#111b21]">
-          <div className="w-1/4 border-r-2 border-gray-600">
-            <div className="bg-[#222e35] text-white text-center py-4 flex flex-col">
-              <div className="float-left ml-3">
-                <ArrowLeftOutlined
-                  className="float-left text-lg cursor-pointer"
-                  onClick={() => onExitChat()}
-                />
-              </div>
-              <p className="before:absolute before:content-[''] before:-translate-x-4 before:translate-y-2 before:h-2 before:w-2 before:bg-green-400 before:rounded-full">
-                Online People
-              </p>
-            </div>
-            <div className="flex justify-center mt-3">
-              <Search
-                placeholder="Search Chat..."
-                className=" bg-white rounded-lg"
-                allowClear
-                style={{ width: 200 }}
+    <div className=" h-screen bg-[#0e151b] p-5">
+      <div className="flex flex-row h-[99%] bg-[#111b21]">
+        <div className="w-1/4 border-r-2 border-gray-600">
+          <div className="bg-[#222e35] text-white text-center py-4 flex flex-col">
+            <div className="float-left ml-3">
+              <ArrowLeftOutlined
+                className="float-left text-lg cursor-pointer"
+                onClick={() => onExitChat()}
               />
             </div>
-            <hr className="mt-3" />
+            <p className="before:absolute before:content-[''] before:-translate-x-4 before:translate-y-2 before:h-2 before:w-2 before:bg-green-400 before:rounded-full">
+              Online People
+            </p>
           </div>
-          <div className="text-white font-mono text-3xl text-center flex justify-center flex-col w-full">
-            <p>Click on Chat!</p>
+          <div className="flex justify-center mt-3">
+            <Search
+              placeholder="Search Chat..."
+              className=" bg-white rounded-lg"
+              allowClear
+              style={{ width: 200 }}
+            />
           </div>
+          <hr className="mt-3" />
+        </div>
+        <div className="text-white font-mono text-3xl text-center flex justify-center flex-col w-full">
+          <p>Click on Chat!</p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -32,78 +32,73 @@ export default function RegisterPage() {
     console.log("Failed:", errorInfo);
   };
   return (
-    <>
-      <div className="backGroundImage bg-cover bg-center flex items-start  justify-around flex-col h-screen">
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="on"
-          className="p-14 border-solid border-2 ms-11 border-red-500 rounded-2xl text-cyan-50 bg-gray-300"
+    <div className="backGroundImage bg-cover bg-center flex items-start  justify-around flex-col h-screen">
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="on"
+        className="p-14 border-solid border-2 ms-11 border-red-500 rounded-2xl text-cyan-50 bg-gray-300"
+      >
+        <Form.Item
+          name={["user", "email"]}
+          rules={[
+            {
+              type: "email",
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
         >
-          <Form.Item
-            name={["user", "email"]}
-            rules={[
-              {
-                type: "email",
-                required: true,
-                message: "Please input your email!",
-              },
-            ]}
-          >
-            <Input
-              className="w-64"
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Email"
-            />
-          </Form.Item>
-          <Form.Item name={["user", "firstname"]} rules={[{ required: true }]}>
-            <Input
-              className="w-64"
-              prefix={<FormOutlined className="site-form-item-icon" />}
-              placeholder="First Name"
-            />
-          </Form.Item>
-          <Form.Item name={["user", "lastname"]} rules={[{ required: true }]}>
-            <Input
-              className="w-64"
-              prefix={<FormOutlined className="site-form-item-icon" />}
-              placeholder="Last Name"
-            />
-          </Form.Item>
+          <Input
+            className="w-64"
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+          />
+        </Form.Item>
+        <Form.Item name={["user", "firstname"]} rules={[{ required: true }]}>
+          <Input
+            className="w-64"
+            prefix={<FormOutlined className="site-form-item-icon" />}
+            placeholder="First Name"
+          />
+        </Form.Item>
+        <Form.Item name={["user", "lastname"]} rules={[{ required: true }]}>
+          <Input
+            className="w-64"
+            prefix={<FormOutlined className="site-form-item-icon" />}
+            placeholder="Last Name"
+          />
+        </Form.Item>
 
-          <Form.Item
-            name="password"
-            rules={[
-              { required: true, message: "Password is Required!" },
-              { min: 8, message: "Password must be minimum 8 characters." },
-            ]}
-          >
-            <Input
-              className="w-64"
-              minLength={8}
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            { required: true, message: "Password is Required!" },
+            { min: 8, message: "Password must be minimum 8 characters." },
+          ]}
+        >
+          <Input
+            className="w-64"
+            minLength={8}
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
 
-          <Form.Item className="flex items-center justify-around">
-            <Button htmlType="submit" className="px-10 bg-amber-950 text-white">
-              Register
-            </Button>
-          </Form.Item>
-          <Link
-            className=" flex justify-around text-rose-700"
-            href="/LoginPage"
-          >
-            Already have an account? Login
-          </Link>
-        </Form>
-      </div>
-    </>
+        <Form.Item className="flex items-center justify-around">
+          <Button htmlType="submit" className="px-10 bg-amber-950 text-white">
+            Register
+          </Button>
+        </Form.Item>
+        <Link className=" flex justify-around text-rose-700" href="/LoginPage">
+          Already have an account? Login
+        </Link>
+      </Form>
+    </div>
   );
 }
